33852b49f361aafae9e52a7f799307fbdc365d74
yogendra jadhav <yj.withyou@gmail.com>
2026-02-14 18:28:23 +0530

    file upload size configured to 100 MB with FormOptions

--- RAGbackend/Program.cs
+++ RAGbackend/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.AspNetCore.Http.Features;
+
 namespace RAGbackend
 {
     public class Program
@@ -20,6 +22,13 @@ namespace RAGbackend
                     policy=>policy.WithOrigins("http://localhost:4200")
                     .AllowAnyHeader()
                     .AllowAnyMethod()));
+
+            // Configure file upload size   
+            builder.Services.Configure<FormOptions>(options =>
+            {
+                options.MultipartBodyLengthLimit = 100 * 1024 * 1024; // 100 MB
+            });
+
 
             var app = builder.Build();
 
