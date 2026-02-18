65369a5d59dfedebb4e875f4ccd563f58ba45a7e
yogendra jadhav <yj.withyou@gmail.com>
2026-02-18 12:49:09 +0530

    file-upload component created.

---
+++ rag-frontend/src/app/components/file-upload/file-upload.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload',
+  imports: [],
+  templateUrl: './file-upload.html',
+  styleUrl: './file-upload.scss',
+})
+export class FileUpload {
+
+}
