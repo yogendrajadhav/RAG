9143f394a0732658cf31bd927e471da91888b84e
yogendra jadhav <yj.withyou@gmail.com>
2026-02-18 16:29:30 +0530

    container created to show file upload and query interface tabs

--- rag-frontend/src/app/app.ts
+++ rag-frontend/src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatIconModule } from '@angular/material/icon';
+import { MatTab, MatTabGroup, MatTabContent } from "@angular/material/tabs";
+import { FileUpload } from "./components/file-upload/file-upload";
+import { QueryInterface } from "./components/query-interface/query-interface";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIconModule, MatTab, MatTabGroup, MatTabContent, FileUpload, QueryInterface],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('RAG-frontend');
+selectedTabIndex=0;
 }
