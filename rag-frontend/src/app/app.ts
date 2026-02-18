14d35b51e0e4a8422919e7bcd07fab72dc6a06c9
yogendra jadhav <yj.withyou@gmail.com>
2026-02-18 12:58:17 +0530

    title property value changed.

--- rag-frontend/src/app/app.ts
+++ rag-frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('rag-frontend');
+  protected readonly title = signal('RAG-frontend');
 }
