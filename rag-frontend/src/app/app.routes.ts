3aef0efc44718c611e3bcfdf0b4f039ae32ad7fc
yogendra jadhav <yj.withyou@gmail.com>
2026-02-18 16:31:12 +0530

    upload and query routes created.

--- rag-frontend/src/app/app.routes.ts
+++ rag-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/upload', loadComponent: () => import('./components/file-upload/file-upload').then(m => m.FileUpload)},
+    { path: '/query', loadComponent: () => import('./components/query-interface/query-interface').then(m => m.QueryInterface)},
+    
+];
+
